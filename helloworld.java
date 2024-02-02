aa2ee8e92884904c57c8b35d04570e4628d2ae60
althafzarar <150312714+althafzarar@users.noreply.github.com>
2024-02-02 08:41:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch32 team with Althaf Kusugal");
+     System.out.println("Hello batch32 team with Althaf Kusugal")
       System.out.println("Hello Batch batch32 WITH FOR STUDENTS RUNNING in front of Althaf Kusugal in making CI automated vikas teach devops CI proecss  ");
     }  
 } 
